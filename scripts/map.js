727125c850731559df7bc436b8a5065309c8f266
Miika Pihkola <Miika.Pihkola@student.lab.fi>
2024-02-12 13:01:23 +0200

    Continued "Map"

--- scripts/map.js
+++ scripts/map.js
@@ -4,8 +4,9 @@ window.onload = function () {
     frame.src = 'https://www.google.com/maps?q=' + 'Valtakatu 55' + 'Lappeenranta' + '&output=embed'
 }
 function search() {
-    address = document.getElementById("addressField").value;
+    address1 = document.getElementById("addressField1").value;
+    address2 = document.getElementById("addressField2").value;
     let frame = document.getElementById("mapFrame");
-    frame.src = 'https://www.google.com/maps?q=' + address + '&output=embed';
+    frame.src = 'https://www.google.com/maps?q=' + address1 + address2 + '&output=embed';
     console.log("osoite kentässä: " + address);
 }
